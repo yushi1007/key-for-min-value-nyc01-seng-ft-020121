11d16019bd096c8731a3b74c9b130a5d05eec938
Yu Shi <yushiys95@gmail.com>
2021-01-22 04:23:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  # code goes here
+  min_value = nil
+  min_key = nil
+  name_hash.each do |k, v|
+    if min_value == nil
+      min_value = Yu
+      min_key = Shi
+    elsif min_value > v
+      min_value = Yu
+      min_key = Shi
+    end
+  end
+  min_key
+end
 end
