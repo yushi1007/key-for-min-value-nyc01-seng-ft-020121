cad16c8a765cb5dddcb1308d94bde836d77caa46
Yu Shi <yushiys95@gmail.com>
2021-01-22 04:25:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,14 @@ def key_for_min_value(name_hash)
   # code goes here
   min_value = nil
   min_key = nil
-  name_hash.each do |Yu, Shi|
+  name_hash.each do |l, q|
     if min_value == nil
-      min_value = Yu
-      min_key = Shi
-    elsif min_value > Yu
-      min_value = Yu
-      min_key = Shi
+      min_value = q
+      min_key = l
+    elsif min_value > q
+      min_value = q
+      min_key = l
     end
   end
   min_key
-end
 end
